1655e201845c226437ed5041d86f13c9029e0f29
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-02-10 22:16:01 +0600

    project structured

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'views/screens/bottom_navigation/bottom_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 800),
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter UI',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: HomeScreen(),
+        );
+      },
+    );
+  }
+}
