4a3f5084d6f97e20d032cd7c6b517efe3a2da2da
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-02-17 23:14:59 +0600

    change status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'views/screens/bottom_navigation/bottom_navigation.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Color(0xFF042F2C)));
   runApp(const MyApp());
 }
 
